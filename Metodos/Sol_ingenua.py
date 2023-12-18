9fff3aa90f164d7bba33cd07b29b1909162dbf60
minizyke <maria.paula.lemus@correounivalle.edu.co>
2023-12-18 12:23:51 -0500

    ada

--- Metodos/Sol_ingenua.py
+++ Metodos/Sol_ingenua.py
@@ -5,17 +5,16 @@ import tkinter as tk
 from tkinter import filedialog
 
 
-def elegir_archivo():
-    ruta_archivo = filedialog.askopenfilename(title="Seleccionar archivo", filetypes=[("Archivos de texto", "*.txt")])
-    if ruta_archivo:
-        datos = leer_datos_desde_archivo(ruta_archivo)
+def seleccionar_archivos():
+    ruta_archivos = filedialog.askopenfilename(title="Seleccionar archivo", filetypes=[("Archivos de texto", "*.txt")])
+    if ruta_archivos:
+        datos = leer_datos_desde_archivo(ruta_archivos)
         if datos:
-            n_equipos, min_partidos, max_partidos = datos
-            distancias = obtener_distancias_desde_archivo(ruta_archivo)
-            calendario = generar_calendario(n_equipos, min_partidos, max_partidos, distancias)
+            num_equipos, min_partidos, max_partidos, distancias = datos
+            calendario = generar_calendario(num_equipos, min_partidos, max_partidos, distancias)
 
             output_text.delete(1.0, tk.END)
-            output_text.insert(tk.END, f"Número de equipos: {n_equipos}\n")
+            output_text.insert(tk.END, f"Número de equipos: {num_equipos}\n")
             output_text.insert(tk.END, f"Mínimo de partidos: {min_partidos}\n")
             output_text.insert(tk.END, f"Máximo de partidos: {max_partidos}\n")
             output_text.insert(tk.END, "Calendario generado:\n")
@@ -34,37 +33,25 @@ def leer_datos_desde_archivo(ruta):
     try:
         with open(ruta, 'r') as archivo:
             lineas = archivo.readlines()
-            n_equipos = int(lineas[0].strip())
+            num_equipos = int(lineas[0].strip())
             min_partidos = int(lineas[1].strip())
             max_partidos = int(lineas[2].strip())
-            return n_equipos, min_partidos, max_partidos
+            distancias = [list(map(int, linea.strip().split())) for linea in lineas[3:]]
+            return num_equipos, min_partidos, max_partidos, distancias
     except Exception as e:
         print(f"Error al leer el archivo: {e}")
         return None
 
 
-def obtener_distancias_desde_archivo(ruta):
-    try:
-        with open(ruta, 'r') as archivo:
-            lineas = archivo.readlines()
-            distancias = [list(map(int, linea.strip().split())) for linea in lineas[3:]]
-            return distancias
-    except Exception as e:
-        print(f"Error al leer las distancias desde el archivo: {e}")
-        return None
+def generar_calendario(num_equipos, min_partidos, max_partidos, distancias):
+    calendario = [[0] * num_equipos for _ in range(2 * (num_equipos - 1))]
+    partidos_jugados = [[False] * num_equipos for _ in range(num_equipos)]
 
+    fecha = 0
+    while fecha < 2 * (num_equipos - 1):
+        equipos_restantes = list(range(1, num_equipos + 1))
 
-def generar_calendario(n_equipos, min_partidos, max_partidos, distancias):
-    if n_equipos % 2 != 0:
-        n_equipos += 1  # Asegurarse de que el número de equipos sea par
-
-    calendario = [[0] * n_equipos for _ in range(2 * (n_equipos - 1))]
-    partidos_jugados = [[False] * n_equipos for _ in range(n_equipos)]
-
-    for fecha in range(n_equipos - 1):
-        equipos_restantes = list(range(1, n_equipos + 1))
-
-        for equipo_local in range(1, n_equipos + 1):
+        for equipo_local in range(1, num_equipos + 1):
             if calendario[fecha][equipo_local - 1] == 0:
                 partidos_positivos = 1
 
@@ -90,7 +77,6 @@ def generar_calendario(n_equipos, min_partidos, max_partidos, distancias):
                     else:
                         break
 
-        # Nueva parte: emparejar equipos restantes
         while equipos_restantes:
             equipo_local = equipos_restantes.pop(0)
             equipo_contrincante = equipos_restantes.pop(0)
@@ -101,20 +87,20 @@ def generar_calendario(n_equipos, min_partidos, max_partidos, distancias):
             partidos_jugados[equipo_local - 1][equipo_contrincante - 1] = True
             partidos_jugados[equipo_contrincante - 1][equipo_local - 1] = True
 
-    # Partidos negativos (invertir partidos positivos para la segunda mitad del calendario)
-    for fecha in range(n_equipos - 1, 2 * (n_equipos - 1)):
-        for equipo in range(n_equipos):
-            calendario[fecha][equipo] = -calendario[fecha - (n_equipos - 1)][equipo]
+        fecha += 1
+
+    for fecha in range(num_equipos - 1, 2 * (num_equipos - 1)):
+        for equipo in range(num_equipos):
+            calendario[fecha][equipo] = -calendario[fecha - (num_equipos - 1)][equipo]
 
     return calendario
-
 
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("Generador y Verificador de Calendario")
 
-    button = tk.Button(root, text="Cargar Archivo", command=elegir_archivo)
+    button = tk.Button(root, text="Cargar Archivo", command=seleccionar_archivos)
     button.pack(pady=20)
 
     output_text = tk.Text(root, height=20, width=50)
